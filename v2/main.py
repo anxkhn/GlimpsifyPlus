555dc96f9ce4d302a674af40f6f9d925d222f601
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2024-10-13 16:27:17 +0530

    chore: minor changes

--- v2/main.py
+++ v2/main.py
@@ -9,19 +9,22 @@ def main():
     ocr_type = "tesseract"
     ocr_strategy = OCRStrategyFactory.create_ocr_strategy(ocr_type)
 
-    extraction_type = "k_transactions"
+    # extraction_type = "k_transactions"
+    extraction_type = "key_moments"
     extraction_strategy = ExtractionStrategyFactory.create_extraction_strategy(
         extraction_type
     )
 
     # input_type = input("Enter input type: ")
-    input_type = "playlist"
+    input_type = "youtube"
+    # input_type = "playlist"
     # input_type = "local"
     # input_type = "object"
     input_strategy: InputStrategy = InputStrategyFactory.create_input_strategy(
         input_type, ocr_strategy, extraction_strategy
     )
     input_strategy.proceed()
+
 
 if __name__ == "__main__":
     main()

5a275b89e04f72e324f5a4c88c661033f9a78bec
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2025-05-03 01:30:21 +0530

    feat: added try catch to playlist

--- v2/playlist_input_strategy.py
+++ v2/playlist_input_strategy.py
@@ -36,26 +36,37 @@ class PlaylistInputStrategy(InputStrategy):
         video_urls = Helper.get_video_urls_from_playlist(self.playlist_url)
         counter = 0
         for video_url in video_urls:
-            print("\n\n\n=====================================")
-            print(f"#{self.start_from + counter} Processing video: {video_url}")
-            print("=====================================")
-            counter += 1
-            if counter < self.start_from:
+            try:
+                print("\n\n\n=====================================")
+                print(f"#{self.start_from + counter} Processing video: {video_url}")
+                print("=====================================")
+                counter += 1
+                if counter < self.start_from:
+                    continue
+
+                # TODO: Ideally, this should not be here. Check if there is a better way to do this.
+                if isinstance(
+                    self.extraction_strategy, KTransactionsExtractionStrategy
+                ):
+                    # TODO: This is a hack. Fix this.
+                    # video_duration = Helper.get_video_duration(video_url)
+                    # number_of_slides = Helper.get_number_of_slides(video_duration)
+                    # self.extraction_strategy.k = number_of_slides
+                    self.extraction_strategy.auto_calculate_k = True
+
+                video_input_strategy = YouTubeVideoURLInputStrategy(
+                    video_url,
+                    self.ocr_strategy,
+                    self.extraction_strategy,
+                    self.ocr_approval_strategy,
+                )
+
+                video_input_strategy.proceed()
+            except Exception:
+                print("Error processing video:", video_url)
+                print("Counter:", counter)
+                with open("error_log.txt", "a") as error_log:
+                    error_log.write(f"Error processing video: {video_url}\n")
+                    error_log.write(f"Counter: {counter}\n")
+                    error_log.write("=====================================\n")
                 continue
-            
-            # TODO: Ideally, this should not be here. Check if there is a better way to do this.
-            if isinstance(self.extraction_strategy, KTransactionsExtractionStrategy):
-                # TODO: This is a hack. Fix this.
-                # video_duration = Helper.get_video_duration(video_url)
-                # number_of_slides = Helper.get_number_of_slides(video_duration)
-                # self.extraction_strategy.k = number_of_slides
-                self.extraction_strategy.auto_calculate_k = True 
-
-            video_input_strategy = YouTubeVideoURLInputStrategy(
-                video_url,
-                self.ocr_strategy,
-                self.extraction_strategy,
-                self.ocr_approval_strategy,
-            )
-
-            video_input_strategy.proceed()

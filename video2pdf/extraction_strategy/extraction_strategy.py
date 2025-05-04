6dab7916676867f78788b1b11467bf1ffdaf9d7e
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2025-05-04 19:09:28 +0530

    feat: made extraction strategy ABC

--- video2pdf/extraction_strategy/extraction_strategy.py
+++ video2pdf/extraction_strategy/extraction_strategy.py
@@ -1,8 +1,11 @@
+from abc import abstractmethod, ABC
 from typing import List
 
 from video2pdf.utils.processed_frame import ProcessedFrame
 
 
-class ExtractionStrategy:
+class ExtractionStrategy(ABC):
+
+    @abstractmethod
     def extract_frames(self, frames: List[ProcessedFrame]) -> List[ProcessedFrame]:
         pass

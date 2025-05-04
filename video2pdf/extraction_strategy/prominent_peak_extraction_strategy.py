22abb1e376eeb644c16693f97609ecbf785ee0d0
Vedant Panchal <60831483+DeveloperDowny@users.noreply.github.com>
2025-05-05 00:12:27 +0530

    feat: updated parameters for prominent_peaks strategy

--- video2pdf/extraction_strategy/prominent_peak_extraction_strategy.py
+++ video2pdf/extraction_strategy/prominent_peak_extraction_strategy.py
@@ -19,7 +19,7 @@ class ProminentPeakExtractionStrategy(BaseExtractionStrategy):
     the chosen parameters.
     """
 
-    def __init__(self, prominence: float = 1.0, distance: int = 1, **kwargs: Any):
+    def __init__(self, prominence: float = 0.1, distance: int = 3, **kwargs: Any):
         """
         Initializes the ProminentPeakExtractionStrategy.
 
